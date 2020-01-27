da31e2bec167b2823cd176a742e26a304419409f
Dmitry Apakin <apakin.d.al@sberbank.ru>
2020-01-28 00:30:57 +0300

    test f()

--- src/components/answers-list-item.jsx
+++ src/components/answers-list-item.jsx
@@ -4,12 +4,16 @@ const AnswersListItem = ({label, isCorrect}) => {
     const style = {
         display: 'inline-block',
         marginRight: '10px',
-        color: isCorrect? 'green' : 'black',
-        fontWeight: isCorrect? 'bold' : 'normal'
+        color: isCorrect ? 'green' : 'black',
+        fontWeight: isCorrect ? 'bold' : 'normal'
+    };
+
+    const f = (message) => {
+        alert(message);
     };
 
     return (
-        <span style={style}>{label}</span>
+        <span style={style} onClick={() => f(label)}>{label}</span>
     );
 };
 
